27260ef03d11e6f0a6dd69fea9036f8f55db8f0c
Demetrio-Aris <85774830+Demetrio-Aris@users.noreply.github.com>
2024-10-07 20:42:09 -0500

    Complete - W02 Team Activity: Dynamic Product Details Page

--- src/js/product.js
+++ src/js/product.js
@@ -1,18 +1,25 @@
-import { setLocalStorage } from './utils.mjs';
-import ProductData from './ProductData.mjs';
+import { getParam } from "./utils.mjs";
+import ProductData from "./ProductData.mjs";
+import ProductDetails from "./ProductDetails.mjs";
 
-const dataSource = new ProductData('tents');
+const dataSource = new ProductData("tents");
+const productId = getParam("product");
 
-function addProductToCart(product) {
-  setLocalStorage('so-cart', product);
-}
-// add to cart button event handler
-async function addToCartHandler(e) {
-  const product = await dataSource.findProductById(e.target.dataset.id);
-  addProductToCart(product);
-}
+const product = new ProductDetails(productId, dataSource);
+product.init();
 
-// add listener to Add to Cart button
-document
-  .getElementById('addToCart')
-  .addEventListener('click', addToCartHandler);
+// console.log(dataSource.findProductById(productId));
+
+// function addProductToCart(product) {
+//   setLocalStorage('so-cart', product);
+// }
+// // add to cart button event handler
+// async function addToCartHandler(e) {
+//   const product = await dataSource.findProductById(e.target.dataset.id);
+//   addProductToCart(product);
+// }
+
+// // add listener to Add to Cart button
+// document
+//   .getElementById('addToCart')
+//   .addEventListener('click', addToCartHandler);
--- src/js/utils.mjs
+++ src/js/utils.mjs
@@ -13,6 +13,14 @@ export function getLocalStorage(key) {
 export function setLocalStorage(key, data) {
   localStorage.setItem(key, JSON.stringify(data));
 }
+
+export function getParams(param) {
+  const queryString = window.location.search;
+  const urlParams = new URLSearchParams(queryString);
+  const product = urlParams.get(param);
+  return product;
+}
+
 // set a listener for both touchend and click
 export function setClick(selector, callback) {
   qs(selector).addEventListener("touchend", (event) => {
@@ -21,3 +29,4 @@ export function setClick(selector, callback) {
   });
   qs(selector).addEventListener("click", callback);
 }
+
