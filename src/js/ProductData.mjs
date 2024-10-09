a05e7449ad9676b731adbd127e55d0f08b63470d
Demetrio-Aris <85774830+Demetrio-Aris@users.noreply.github.com>
2024-10-08 20:17:14 -0500

    update

--- src/js/ProductData.mjs
+++ src/js/ProductData.mjs
@@ -1,23 +1,33 @@
 function convertToJson(res) {
-  if (res.ok) {
-    return res.json();
-  } else {
+  if (!res.ok) {
     throw new Error("Bad Response");
   }
+  return res.json();
 }
 
 export default class ProductData {
   constructor(category) {
     this.category = category;
-    this.path = `/json/${this.category}.json`;
+    this.path = `../json/${this.category}.json`;
   }
-  getData() {
-    return fetch(this.path)
-      .then(convertToJson)
-      .then((data) => data);
+
+  async getData() {
+    try {
+      const response = await fetch(this.path);
+      return convertToJson(response);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+      throw error;
+    }
   }
+
   async findProductById(id) {
-    const products = await this.getData();
-    return products.find((item) => item.Id === id);
+    try {
+      const products = await this.getData();
+      return products.find((item) => item.Id === id);
+    } catch (error) {
+      console.error(`Error finding product with id: ${id}`, error);
+      return null;
+    }
   }
 }
